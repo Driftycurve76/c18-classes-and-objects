829eb462e84b5dc6f104b0371962262b47f8ad14
Driftycurve76 <86668070+Driftycurve76@users.noreply.github.com>
2021-09-16 19:38:38 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var s1
+var s2
+function setup() {
+  createCanvas(400,400);
+s1 = new Student("Matthew", 12, 7);
+s2 = new Student("Bryan", 12, 7)
+s1.display();
+s2.display();
+}
+
+function draw() 
+{
+  background(30);
+  
+}
